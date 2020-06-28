440084cbae63e81321bddffc424a4d058d8028bb
Tigri <imbert418@gmail.com>
2020-06-28 14:19:29 +0200

    (fix) Crawler: Rename file for consistency

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const Crawler = require('./src/back/CrawlerPuppet');
+const Crawler = require('./src/back/Crawler');
 const Logger = require('./src/back/Logger');
 const Queries = require('./src/back/database/Queries');
 
